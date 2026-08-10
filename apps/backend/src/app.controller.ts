0b6aa822a953b1703813345f2054590ddb1aa48a
ryo-i <iidazz0109@gmail.com>
2026-08-10 11:38:56 +0900

    ホットリロード機能の追加

--- apps/backend/src/app.controller.ts
+++ apps/backend/src/app.controller.ts
@@ -5,6 +5,11 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Get('health')
+  getHealth() {
+    return { status: 'ok' };
+  }
+
   @Get('persons')
   async getPersons() {
     return await this.appService.getPersons();
